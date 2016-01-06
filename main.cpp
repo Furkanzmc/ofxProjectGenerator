3105c4344493bd0df1810dad0e656eee6f480939
Furkan Üzümcü <furkan.uzumcu@edmoware.com>
2016-01-06 19:17:06 +0200

    Add app info

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QApplication::setApplicationName("ofProjectGenerator");
+    QApplication::setApplicationVersion("1.0.1");
+    QApplication::setOrganizationName("Furkanzmc");
+
     MainWindow w;
     w.show();
 
