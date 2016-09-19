6cc70c0cbdb42143626f9af659bafb5f67e6b6b6
Furkan Üzümcü <furkanuzumcu@gmail.com>
2016-09-19 10:48:45 +0300

    Add space after #

--- data/findOpenFrameworks-v0.9.cmake
+++ data/findOpenFrameworks-v0.9.cmake
@@ -6,7 +6,8 @@ set(ADDONS_LIBS "")
 set(ADDONS_INCLUDE_PATH "")
 set(ADDONS_PATH ${OF_PATH}/addons)
 set(USE_PRECOMPILED_LIB true)
-#This can also be Win32
+
+# This can also be Win32
 if(AR STREQUAL "x64" OR AR STREQUAL "Win32")
     message("NOTICE [openFrameworks]: Compiling openFrameworks for ${AR}")
 else()
@@ -66,7 +67,7 @@ if(MSVC)
     set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OF_LINKER_FLAGS}")
 endif(MSVC)
 
-#BOOST
+# BOOST
 if(DEBUG_MODE)
     list(APPEND OF_LIBS
         ${OF_PATH}/libs/boost/lib/vs/${AR}/libboost_filesystem-vc140-mt-gd-1_58.lib
@@ -84,7 +85,7 @@ endif(DEBUG_MODE)
         )
 
 
-#CAIRO
+# CAIRO
 list(APPEND OF_LIBS ${OF_PATH}/libs/cairo/lib/vs/${AR}/cairo-static.lib)
 list(APPEND OF_LIBS ${OF_PATH}/libs/cairo/lib/vs/${AR}/libpng.lib)
 list(APPEND OF_LIBS ${OF_PATH}/libs/cairo/lib/vs/${AR}/pixman-1.lib)
@@ -95,7 +96,7 @@ list(APPEND OF_INCLUDE_PATH ${OF_PATH}/libs/cairo/include/pixman-1)
 list(APPEND OF_INCLUDE_PATH ${OF_PATH}/libs/cairo/include/cairo)
 
 
-#FMOEX
+# FMOEX
 if (AR STREQUAL "x64")
     list(APPEND OF_LIBS ${OF_PATH}/libs/fmodex/lib/vs/${AR}/fmodex64_vc.lib)
 else()
@@ -104,12 +105,12 @@ endif()
 list(APPEND OF_INCLUDE_PATH ${OF_PATH}/libs/fmodex/include)
 
 
-#FREEIMAGE
+# FREEIMAGE
 list(APPEND OF_LIBS ${OF_PATH}/libs/FreeImage/lib/vs/${AR}/FreeImage.lib)
 list(APPEND OF_INCLUDE_PATH ${OF_PATH}/libs/FreeImage/include)
 
 
-#FREETYPE
+# FREETYPE
 list(APPEND OF_LIBS ${OF_PATH}/libs/freetype/lib/vs/${AR}/libfreetype.lib)
 list(APPEND OF_INCLUDE_PATH
     ${OF_PATH}/libs/freetype/include
@@ -117,12 +118,12 @@ list(APPEND OF_INCLUDE_PATH
     )
 
 
-#GLEW
+# GLEW
 list(APPEND OF_LIBS ${OF_PATH}/libs/glew/lib/vs/${AR}/glew32s.lib)
 list(APPEND OF_INCLUDE_PATH ${OF_PATH}/libs/glew/include)
 
 
-#GLFW
+# GLFW
 list(APPEND OF_LIBS ${OF_PATH}/libs/glfw/lib/vs/${AR}/glfw3.lib)
 list(APPEND OF_INCLUDE_PATH
     ${OF_PATH}/libs/glfw/include
@@ -130,24 +131,24 @@ list(APPEND OF_INCLUDE_PATH
     )
 
 
-#GLU
+# GLU
 list(APPEND OF_LIBS ${OF_PATH}/libs/glu/lib/vs/Win32/glu32.lib)
 list(APPEND OF_INCLUDE_PATH ${OF_PATH}/libs/glu/include)
 
 
-#GLUT
+# GLUT
 list(APPEND OF_LIBS ${OF_PATH}/libs/glut/lib/vs/${AR}/glut32.lib)
 list(APPEND OF_INCLUDE_PATH ${OF_PATH}/libs/glut/include)
 
 
-#OPENSSL
+# OPENSSL
 list(APPEND OF_LIBS ${OF_PATH}/libs/openssl/lib/vs/${AR}/libeay32md.lib)
 list(APPEND OF_LIBS ${OF_PATH}/libs/openssl/lib/vs/${AR}/ssleay32md.lib)
 list(APPEND OF_INCLUDE_PATH ${OF_PATH}/libs/openssl/include)
 list(APPEND OF_INCLUDE_PATH ${OF_PATH}/libs/openssl/include/openssl)
 
 
-#POCO
+# POCO
 if(DEBUG_MODE)
     list(APPEND OF_LIBS
         ${OF_PATH}/libs/poco/lib/vs/${AR}/PocoCryptomdd.lib
@@ -172,14 +173,14 @@ endif(DEBUG_MODE)
     list(APPEND OF_INCLUDE_PATH ${OF_PATH}/libs/poco/include)
 
 
-#QUICKTIME
+# QUICKTIME
 list(APPEND OF_LIBS ${OF_PATH}/libs/quicktime/lib/vs/Win32/qtmlClient.lib)
 list(APPEND OF_LIBS ${OF_PATH}/libs/quicktime/lib/vs/Win32/QTSClient.lib)
 list(APPEND OF_LIBS ${OF_PATH}/libs/quicktime/lib/vs/Win32/Rave.lib)
 list(APPEND OF_INCLUDE_PATH ${OF_PATH}/libs/quicktime/include)
 
 
-#RTAUDIO
+# RTAUDIO
 if(DEBUG_MODE)
     list(APPEND OF_LIBS ${OF_PATH}/libs/rtAudio/lib/vs/${AR}/rtAudioD.lib)
 else()
@@ -188,19 +189,19 @@ endif(DEBUG_MODE)
     list(APPEND OF_INCLUDE_PATH ${OF_PATH}/libs/rtAudio/include)
 
 
-#TESS2
+# TESS2
 list(APPEND OF_LIBS ${OF_PATH}/libs/tess2/lib/vs/${AR}/tess2.lib)
 list(APPEND OF_INCLUDE_PATH ${OF_PATH}/libs/tess2/include)
 
 
-#UTF8CPP
+# UTF8CPP
 list(APPEND OF_INCLUDE_PATH
     ${OF_PATH}/libs/utf8cpp/include
     ${OF_PATH}/libs/utf8cpp/include/utf8
     )
 
 
-#VIDEO_INPUT
+# VIDEO_INPUT
 if(DEBUG_MODE)
     list(APPEND OF_LIBS ${OF_PATH}/libs/videoInput/lib/vs/${AR}/videoInputD.lib)
 else()
@@ -209,7 +210,7 @@ endif(DEBUG_MODE)
     list(APPEND OF_INCLUDE_PATH ${OF_PATH}/libs/videoInput/include)
 
 
-#SYSTEM LIBS
+# SYSTEM LIBS
 find_library(MSIMG32 msimg32)
 find_library(OPEN_GL_32 OpenGL32)
 find_library(KERNEL_32 kernel32)
