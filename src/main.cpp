a3f0fe7bb2c6b0c8bb84616aed63bcf86797f282
Furkan Üzümcü <furkanuzumcu@gmail.com>
2017-02-01 18:34:14 +0300

    Change version

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QApplication::setApplicationName("ofProjectGenerator");
-    QApplication::setApplicationVersion("0.5");
+    QApplication::setApplicationVersion("0.5.1");
     QApplication::setOrganizationName("Furkanzmc");
 
     MainWindow w;
